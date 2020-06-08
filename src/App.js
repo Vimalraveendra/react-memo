d9b6f1b24eee1583fbdb76260ff2464d31640d69
vimal Raveendran <vimalraveendra@gmail.com>
2020-06-08 12:06:12 +0200

    Updated App compont with a button element and count state also updated the styles of App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-
+import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
   render() {
+    const { count } = this.state;
     return (
       <div className="App">
-        <h2>Hello</h2>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          Button Count:{count}
+          <button
+            className="btn"
+            onClick={() => this.setState({ count: count + 1 })}
+          >
+            Increase Count
+          </button>
+        </header>
       </div>
     );
   }
